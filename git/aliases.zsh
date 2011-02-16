2d15ac5e6e28326df3c38770bfecd712d4ef4ac2
Tim Clem <timothy.clem@gmail.com>
2011-02-15 17:35:48 -0800

    altering git path for homebrew install and rvm

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,17 +9,21 @@
 # syntax is way too slow). It should work fine for those without rvm, though.
 if [[ -s $HOME/.rvm/scripts/rvm ]] 
 then
-  alias git='$HOME/.rvm/rubies/ruby-1.8.7-p302/bin/ruby `which hub`'
+  # alias git='$HOME/.rvm/rubies/ruby-1.8.7-p302/bin/ruby `which hub`'
+  alias git='$HOME/.rvm/rubies/ruby-1.8.7-p330/bin/ruby `which hub`'
 fi
 
 # The rest of my fun git aliases
+alias ga='git add .'
 alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias gitc='git commit -m'
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gss='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
