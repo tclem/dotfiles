cbebb93f60625296d4c68a295e1036e58fc58ed0
Tim Clem <timothy.clem@gmail.com>
2011-03-08 16:17:28 -0800

    add lock-screen, updated ga alias to func

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,7 +14,11 @@ then
 fi
 
 # The rest of my fun git aliases
-alias ga='git add .'
+# alias ga='git add .'
+ga() {
+ git add .
+ git add -u
+}
 alias gl='git pull --rebase --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
