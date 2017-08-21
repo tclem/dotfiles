db45076b4fb314f456cfc2579e5ff5e27f4bd2c4
Timothy Clem <timothy.clem@gmail.com>
2017-08-21 07:43:14 -0700

    Update path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
-export PATH=".:bin:$HOME/.local/bin:/usr/local/bin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH=".:bin:$HOME/.cabal/bin:$HOME/.local/bin:/usr/local/bin:$ZSH/bin:$PATH"
