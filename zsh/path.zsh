2a64020d78d2ac920c98a0edb3303b837f2475df
Timothy Clem <timothy.clem@gmail.com>
2017-12-01 08:03:50 -0800

    Remove ~/.cabal/bin from path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,2 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH=".:bin:$HOME/.cabal/bin:$HOME/.local/bin:/usr/local/bin:$ZSH/bin:$PATH"
+export PATH=".:bin:$HOME/.local/bin:/usr/local/bin:$ZSH/bin:$PATH"
