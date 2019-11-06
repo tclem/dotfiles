7559b82d818b102db6f5df100f9d23a6f876f6b3
Timothy Clem <timothy.clem@gmail.com>
2019-11-06 09:03:18 -0800

    zsh setup fixes

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -32,7 +32,6 @@ setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY # adds history incrementally
-# setopt SHARE_HISTORY  # share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
