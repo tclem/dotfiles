7d9e59af1a65c5096314d83198c9fc90331960a5
Timothy Clem <timothy.clem@gmail.com>
2020-09-21 09:42:38 -0700

    New aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,8 @@
 alias dotmate='mate $ZSH'
 alias dotcd='cd $ZSH'
 alias reload!='. ~/.zshrc'
-alias clean-local-branches='git branch --merged master | grep -v '\''master$'\'' | xargs git branch -d'
+alias clean-local-branches='git branch --merged main | grep -v ''main$'' | xargs git branch -d'
+alias clean-local-branchesm='git branch --merged master | grep -v '\''master$'\'' | xargs git branch -d'
 alias mxk="tmux kill-session -t"
 alias pu='pushd'
 alias po='popd'
@@ -12,6 +13,9 @@ alias lsa='ls -lah'
 alias l='ls -lha'
 alias ll='ls -lh'
 alias tags='ctags --extra=+f -R .'
+alias fix-camera='sudo killall VDCAssistant'
+alias no-sleep='sudo pmset -b sleep 0; sudo pmset -b disablesleep 1' # don't sleep with lid closed
+alias sleep-again='sudo pmset -b sleep 5; sudo pmset -b disablesleep 0'
 
 # Git
 alias ga='git add --all'
@@ -31,7 +35,7 @@ alias be="bundle exec"
 alias sc='script/console'
 alias ss='script/server'
 alias st='script/test'
-alias t='bin/testrb'
+alias t='bin/rails test'
 alias migrate='rake db:migrate db:test:prepare'
 alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
 alias killnginx="ps aux | grep nginx | awk '{print $2}' | xargs sudo kill -9"
