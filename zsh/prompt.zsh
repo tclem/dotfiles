f45c3d65191d214518beae85a7300aeb0834871d
Timothy Clem <timothy.clem@gmail.com>
2021-12-08 18:19:27 -0800

    Working on faster git prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,18 +3,21 @@ autoload colors && colors
 # http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
 git_branch() {
-  echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  # echo $(git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  echo $(git symbolic-ref HEAD 2>/dev/null | sed -E 's/refs\/(heads|tags)\///')
 }
 
 git_dirty() {
-  # if [[ $st == "" ]]
-  st=$(git status 2>/dev/null | tail -n 1)
-  if [[ $st =~ 'fatal: Not a git repository' ]]
-  then
+  if [[ $(git remote get-url origin) == "git@github.com:github/github" ]]; then
+    st=$(git status --no-ahead-behind 2>/dev/null | tail -n 1)
+  else
+    st=$(git status 2>/dev/null | tail -n 1)
+  fi
+
+  if [[ $st =~ 'fatal: Not a git repository' ]]; then
     echo ""
   else
-    if [[ $st =~ 'nothing to commit' ]]
-    then
+    if [[ $st =~ 'nothing to commit' ]]; then
       echo "on %{$fg[green]%}$(git_prompt_info)%{$reset_color%}"
     else
       echo "on %{$fg[red]%}$(git_prompt_info)%{$reset_color%}"
@@ -22,40 +25,41 @@ git_dirty() {
   fi
 }
 
-git_prompt_info () {
+git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2>/dev/null) || return
   echo "${ref#refs/heads/}"
 }
 
-project_name () {
+project_name() {
   name=$(pwd | awk -F'GitHub/' '{print $2}' | awk -F/ '{print $1}')
   echo $name
 }
 
-project_name_color () {
+project_name_color() {
   echo "%{\e[0;35m%}${name}%{\e[0m%}"
 }
 
-unpushed () {
+unpushed() {
   git cherry -v origin/$(git_branch) 2>/dev/null
 }
 
-need_push () {
-  if [[ $(unpushed) == "" ]]
-  then
+need_push() {
+  if [[ $PWD == "$HOME/github/github" ]]; then
     echo ""
   else
-    echo " with %{$fg[magenta]%}unpushed%{$reset_color%}"
+    if [[ $(unpushed) == "" ]]; then
+      echo ""
+    else
+      echo " with %{$fg[magenta]%}unpushed%{$reset_color%}"
+    fi
   fi
 }
 
-ruby_prompt(){
+ruby_prompt() {
   rv=$(rbenv version-name)
-  if (echo $rv &> /dev/null)
-  then
+  if (echo $rv &>/dev/null); then
     echo "%{$fg[yellow]%}ruby $rv%{$reset_color%}"
-  elif $(which rvm &> /dev/null)
-  then
+  elif $(which rvm &>/dev/null); then
     echo "%{$fg[yellow]%}$(rvm tools identifier)%{$reset_color%}"
   else
     echo ""
@@ -73,7 +77,7 @@ function check_last_exit_code() {
   fi
 }
 
-directory_name(){
+directory_name() {
   echo "%{$fg[cyan]%}%1/%\/%{$reset_color%}"
 }
 
