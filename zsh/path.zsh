c7114fc27fc4fc9086ee42bc50dac1e8d585f59a
Timothy Clem <timothy.clem@gmail.com>
2021-12-08 18:19:55 -0800

    What do I know about Python?

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,9 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export PATH="/usr/local/bin:$ZSH/bin:$PATH"
-export PATH="$HOME/.cargo/bin:$PATH"  # Rust cargo
+export PATH="$HOME/.cargo/bin:$PATH" # Rust cargo
 export PATH="$HOME/.local/bin:$PATH" # stack
 export PATH="$HOME/.ghcup/bin:$PATH" # ghcup
 export PATH="$HOME/.cabal/bin:$PATH" # cabal
 export PATH=":bin:$PATH"
 export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+export PATH="/usr/local/opt/python@3.9/libexec/bin:$PATH"
