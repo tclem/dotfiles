5017a7a02c851bfa4ddfe28844951d9682112a1e
Timothy Clem <timothy.clem@gmail.com>
2024-04-20 12:34:35 -0700

    Latest path updates

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,9 +1,14 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-export PATH="/usr/local/bin:$ZSH/bin:$PATH"
+export PATH="/usr/local/bin:$ZSH/bin:$PATH" # homebrew
+export PATH=$PATH:$HOME/go/bin # Go binaries
 export PATH="$HOME/.cargo/bin:$PATH" # Rust cargo
-export PATH="$HOME/.local/bin:$PATH" # stack
-export PATH="$HOME/.ghcup/bin:$PATH" # ghcup
-export PATH="$HOME/.cabal/bin:$PATH" # cabal
 export PATH=":bin:$PATH"
-export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
-export PATH="/usr/local/opt/python@3.9/libexec/bin:$PATH"
+
+export PATH="/usr/local/opt/mysql-client/bin:$PATH" # mysql
+
+# export PATH="/usr/local/opt/python@3.8/libexec/bin:$PATH"
+
+# node and npm, specifically not using a version manager as I only work with
+# node in the tree-sitter project.
+# export PATH="/usr/local/opt/node@10/bin:$PATH"
+# export PATH="$PATH:./node_modules/.bin"
