5f79cc1092d0aec77faab37e4326c6cd4d265a2d
Timothy Clem <timothy.clem@gmail.com>
2024-05-31 04:31:32 +0000

    Install once

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 
 set -euo pipefail
 
+if [[ -f ~/.dotfilesv && $(cat ~/.dotfilesv) == $(git rev-parse HEAD) ]]; then
+  exit 1
+fi
+
 if [[ "$(uname -s)" = "Darwin" ]]; then
     # ./install-macos.sh
     echo "This is a good time to create a GitHub PAT, create ~/.netrc and ~/.localrc"
@@ -12,11 +16,11 @@ else
     echo "Linux install is a WIP"
 fi
 
+gh alias import gh/aliases.yml
+
 # Setup .zshrc
 rm -rf ~/.zshrc
 ln -s "$(pwd)/.zshrc" ~/.zshrc
-
-gh alias import gh/aliases.yml
 
 # Ruby
 # git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
@@ -40,3 +44,6 @@ git config --global alias.last 'log -1 HEAD'
 git config --global alias.first 'log --reverse --pretty=format:"%h %ad%x09%an%x09%s" --date=short | grep Clem | head -1'
 git config --global pull.rebase true
 git config --global push.default simple
+
+# write out a manifest of the installed dotfiles version: delete this file to force a re-install
+git rev-parse HEAD > ~/.dotfilesv
